7f16ca23ae0487f55eefe4265cb28ea32c6fa3f4
Javier Pascal <PascalJa@basfad.basf.net>
2021-03-09 08:40:13 -0600

    add image and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeadingComponent } from './components/common/heading/heading.component';
+import { TankTableComponent } from './components/tank-table/tank-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadingComponent,
+    TankTableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/tank-table/tank-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tank-table',
+  templateUrl: './tank-table.component.html',
+  styleUrls: ['./tank-table.component.css']
+})
+export class TankTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
